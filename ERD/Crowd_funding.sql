596aef3acd626be2b371ffc78be8aa27be6ede51
Denis <kalaladm@mail.uc.edu>
2025-03-20 22:03:50 -0400

    fixed the issues with varchar error and forgot to add last name to the contacts.

--- ERD/Crowd_funding.sql
+++ ERD/Crowd_funding.sql
@@ -1,14 +1,14 @@
 CREATE TABLE "campaign" (
     "cf_id" INT   NOT NULL,
     "contact_id" INT   NOT NULL,
-    "company_name" VARCHAR(30)   NOT NULL,
+    "company_name" VARCHAR(255)   NOT NULL,
     "description" TEXT   NOT NULL,
     "goal" FLOAT   NOT NULL,
     "pledged" FLOAT   NOT NULL,
-    "outcome" VARCHAR(30)   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
     "backers_count" INT   NOT NULL,
-    "country" VARCHAR(10)   NOT NULL,
-    "currency" VARCHAR(10)   NOT NULL,
+    "country" VARCHAR(50)   NOT NULL,
+    "currency" VARCHAR(50)   NOT NULL,
     "launch_date" DATE   NOT NULL,
     "end_date" DATE   NOT NULL,
     "category_id" VARCHAR   NOT NULL,
@@ -19,16 +19,16 @@ CREATE TABLE "campaign" (
 );
 
 CREATE TABLE "category" (
-    "category_id" VARCHAR(30)   NOT NULL,
-    "category" VARCHAR(30)   NOT NULL,
+    "category_id" VARCHAR(50)   NOT NULL,
+    "category" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR(30)   NOT NULL,
-    "subcategory" VARCHAR(30)   NOT NULL,
+    "subcategory_id" VARCHAR(50)   NOT NULL,
+    "subcategory" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -36,8 +36,9 @@ CREATE TABLE "subcategory" (
 
 CREATE TABLE "contacts" (
     "contact_id" INT   NOT NULL,
-    "first_name" VARCHAR(30)   NOT NULL,
-    "email" VARCHAR(50)   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
